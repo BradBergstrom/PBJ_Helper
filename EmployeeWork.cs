254811b3f02b5ada2417ed6666e26cba3b8c317b
Bradley Bergstrom <bbergst@gmail.com>
2023-02-22 22:05:18 -0800

    init commit

---
+++ EmployeeWork.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PBJ_Helper.WorkDay;
+
+namespace PBJ_Helper
+{
+    public class WorkDay
+    {
+        public enum DayOfWeek
+        {
+            sun,
+            mon,
+            tue,
+            wed,
+            thu,
+            fri,
+            sat,
+            sun_1,
+            mon_1,
+            tue_1,
+            wed_1,
+            thu_1,
+            fri_1,
+            sat_1
+        }
+        public WorkDay(int dayOfMonth, DayOfWeek dayOfWeek)
+        {
+            this.dayOfMonth = dayOfMonth;
+            this.dayOfWeek = dayOfWeek;
+        }
+        public DayOfWeek dayOfWeek;
+        public int dayOfMonth { get; set; }
+        public string punches;
+        public double lunchDeductions;
+        public double Reg;
+        public double OT;
+        public double totalHours;
+
+        public double correctedHours { get; set; }
+    }
+    public class EmployeeWork
+    {
+        public string name { get; set; }
+        public string employeeId { get; set; }
+        public string department { get; set; }
+        public List<WorkDay> daysWorked;
+
+        public double calcWeek1Hours()
+        {
+            double week1Total = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                week1Total += daysWorked[i].correctedHours;
+            }
+            return week1Total;
+        }
+        public double calcWeek2Hours()
+        {
+            double week2Total = 0;
+            for (int i = 7; i < 14; i++)
+            {
+                week2Total += daysWorked[i].correctedHours;
+            }
+            return week2Total;
+        }
+        public WorkDay getWorkday(WorkDay.DayOfWeek day)
+        {
+            return daysWorked[(int)day];
+        }
+        public void loadWorkWeek(DataRow daysOfMonth, DataRow punches, DataRow lunchDeductions, DataRow reg, DataRow OT)
+        {
+            daysWorked = new List<WorkDay>();
+            WorkDay tempDay;
+            WorkDay.DayOfWeek currentDayOfWeek = WorkDay.DayOfWeek.sun;
+            for (int i = 3; i < daysOfMonth.ItemArray.Count(); i++)
+            {
+                if (!(daysOfMonth.ItemArray[i] is System.DBNull))
+                {
+                    tempDay = new WorkDay(Int32.Parse(daysOfMonth.ItemArray[i].ToString()), currentDayOfWeek);
+                    currentDayOfWeek++;
+                    if (punches != null && !(punches.ItemArray[i] is System.DBNull))
+                    {
+                        tempDay.punches = punches.ItemArray[i].ToString();
+                    }
+
+                    if (lunchDeductions != null && !(lunchDeductions.ItemArray[i] is System.DBNull))
+                    {
+                        tempDay.lunchDeductions = float.Parse(lunchDeductions.ItemArray[i].ToString());
+                    }
+
+                    if (reg != null && !(reg.ItemArray[i] is System.DBNull))
+                    {
+                        tempDay.Reg = float.Parse(reg.ItemArray[i].ToString());
+                    }
+
+                    if (OT != null && !(OT.ItemArray[i] is System.DBNull))
+                    {
+                        tempDay.OT = float.Parse(OT.ItemArray[i].ToString());
+                    }
+                    daysWorked.Add(tempDay);
+
+                }
+            }
+
+            //we need all the days to be filled in before correcting the hours. Can't include this in the above loop sadly.
+            double totalWeekHours = 0;
+            for (int i = 0; i < daysWorked.Count; i++)
+            {
+                correctHours(daysWorked[i]);
+                totalWeekHours += daysWorked[i].correctedHours;
+            }
+
+            calcWeek1Hours();
+            calcWeek2Hours();
+
+            int derp = 5;
+        }
+        public void correctHours(WorkDay day)
+        {
+            if (day.punches != null)
+            {
+                WorkDay nextDay = daysWorked.FirstOrDefault(d => d.dayOfWeek - 1 == day.dayOfWeek);
+
+                string[] punchTimes = day.punches.Split(new string[] { "\n" }, StringSplitOptions.None);
+                string startDateTime = punchTimes[0];
+                string stopDateTime = punchTimes[1];
+
+                DateTime startTime = DateTime.Parse(punchTimes[0]); // No error checking
+                DateTime endTime = DateTime.Parse(punchTimes[1]); // No error checking
+                if (startDateTime.Contains("PM") && stopDateTime.Contains("AM"))
+                {
+                    DateTime midnight = DateTime.Parse("12:00 AM"); // No error checking
+                    midnight = midnight.AddDays(1);
+                    TimeSpan timeWorkedToday = midnight - startTime;
+
+                    day.correctedHours += timeWorkedToday.TotalHours; //if a previous day added hours here, Don't want to overwrite that
+                    day.correctedHours -= day.lunchDeductions;
+
+                    if (nextDay != null)
+                    {
+                        endTime = endTime.AddDays(1);
+                        TimeSpan timeWorkedTomorrow = endTime - midnight;
+                        nextDay.correctedHours += timeWorkedTomorrow.TotalHours;                        
+                    }
+                }
+                else
+                {
+                    TimeSpan timeWorkedToday = endTime - startTime;
+                    day.correctedHours += timeWorkedToday.TotalHours; //if a previous day added hours here, Don't want to overwrite that
+                    day.correctedHours -= day.lunchDeductions;
+                }
+            }
+        }
+    }
+    public class FullEmpoyeeOutput
+    {
+        public FullEmpoyeeOutput(EmployeeWork employee)
+        {
+            name = employee.name;
+            employeeId = employee.employeeId;
+            department = employee.department;
+
+            Sun = employee.daysWorked[0].correctedHours;
+            Mon = employee.daysWorked[1].correctedHours;
+            Tue = employee.daysWorked[2].correctedHours;
+            Wed = employee.daysWorked[3].correctedHours;
+            Thu = employee.daysWorked[4].correctedHours;
+            Fri = employee.daysWorked[5].correctedHours;
+            Sat = employee.daysWorked[6].correctedHours;
+
+            week1Total = employee.calcWeek1Hours();
+
+            Sun_2 = employee.daysWorked[7].correctedHours;
+            Mon_2 = employee.daysWorked[8].correctedHours;
+            Tue_2 = employee.daysWorked[9].correctedHours;
+            Wed_2 = employee.daysWorked[10].correctedHours;
+            Thu_2 = employee.daysWorked[11].correctedHours;
+            Fri_2 = employee.daysWorked[12].correctedHours;
+            Sat_2 = employee.daysWorked[13].correctedHours;
+            
+            week2Total = employee.calcWeek2Hours();
+        }
+        public string name { get; set; }
+        public string employeeId { get; set; }
+        public string department { get; set; }
+        public double Sun { get; set; }
+        public double Mon { get; set; }
+        public double Tue { get; set; }
+        public double Wed { get; set; }
+        public double Thu { get; set; }
+        public double Fri { get; set; }
+        public double Sat { get; set; }
+        public double week1Total { get; set; }
+        public double Sun_2 { get; set; }
+        public double Mon_2 { get; set; }
+        public double Tue_2 { get; set; }
+        public double Wed_2 { get; set; }
+        public double Thu_2 { get; set; }
+        public double Fri_2 { get; set; }
+        public double Sat_2 { get; set; }
+        public double week2Total { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,154 @@
+﻿using ExcelDataReader;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PBJ_Helper
+{
+    public class Program
+    {
+        static List<FullEmpoyeeOutput> employees;
+        public static void ToJson(string excelFileName, string outputFileName)
+        {
+            employees = new List<FullEmpoyeeOutput>();
+
+            using (var stream = File.Open(excelFileName, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    var dataSet = reader.AsDataSet(new ExcelDataSetConfiguration()
+                    {
+                        UseColumnDataType = true,
+                        ConfigureDataTable = (tableReader) => new ExcelDataTableConfiguration()
+                        {
+                            UseHeaderRow = true
+                        }
+                    });
+
+                    EmployeeWork currentEmployee = null;
+                    bool nextRowIsDates = false;
+                    DataRow punchesRow = null;
+                    DataRow lunchDeductionRow = null;
+                    DataRow regRow = null;
+                    DataRow otRow = null;
+
+                    foreach (DataTable table in dataSet.Tables)
+                    {
+                        for(int i = 0; i < table.Rows.Count; i++)
+                        {
+                            DataRow row = table.Rows[i];
+                            if (row.ItemArray[1].ToString() == "Name:")
+                            {
+                                currentEmployee = new EmployeeWork();
+                                currentEmployee.name = row.ItemArray[2].ToString();
+                            }
+                            else if (row.ItemArray[1].ToString() == "Employee Id:")
+                            {
+                                currentEmployee.employeeId = row.ItemArray[2].ToString();
+                            }
+                            else if (row.ItemArray[1].ToString() == "Department:")
+                            {
+                                currentEmployee.department = row.ItemArray[2].ToString();
+                            }
+                            //if row sunday contains value 'Sun', then the next row will be the dates
+                            else if (row.ItemArray[3].ToString() == "Sun")
+                            {
+                                nextRowIsDates = true;
+                                int manualRowIndex = 1; //if the row we are expecting is next, move the counter. otherwise assume this person does not have that row
+                                DataRow daysOfMonthRow = table.Rows[i + manualRowIndex];
+                                if(table.Rows[i + manualRowIndex + 1].ItemArray[1].ToString() == "Punches")
+                                {
+                                    manualRowIndex++; //2
+                                    punchesRow = table.Rows[i + manualRowIndex];
+                                }
+                                if (table.Rows[i + manualRowIndex + 1].ItemArray[1].ToString() == "Lunch Deductions")
+                                {
+                                    manualRowIndex++; //3
+                                    lunchDeductionRow = table.Rows[i + manualRowIndex];
+                                }
+                                if (table.Rows[i + manualRowIndex + 1].ItemArray[1].ToString() == "Reg")
+                                {
+                                    manualRowIndex++; //4
+                                    regRow = table.Rows[i + manualRowIndex];
+                                }
+                                if (table.Rows[i + manualRowIndex + 1].ItemArray[1].ToString() == "OT")
+                                {
+                                    manualRowIndex++; //5
+                                    otRow = table.Rows[i + manualRowIndex];
+                                }
+
+                                currentEmployee.loadWorkWeek(daysOfMonthRow, punchesRow, lunchDeductionRow, regRow, otRow);
+                                daysOfMonthRow = null; punchesRow = null; lunchDeductionRow = null; regRow = null; otRow = null;
+                                i += manualRowIndex;
+                                employees.Add(new FullEmpoyeeOutput(currentEmployee));
+                            }
+                        }
+                        int derp = 3;
+                    }
+
+                    //string csv = String.Join(",", employees.Select(x => x.ToString()).ToArray());
+
+                    PropertyInfo[] properties = typeof(FullEmpoyeeOutput).GetProperties();
+                    CreateCSV(employees, outputFileName);
+
+                    //File.WriteAllText(
+                    //    jsonFileName,
+                    //    JsonConvert.SerializeObject(dataSet.Tables[0],
+                    //        Formatting.Indented));
+                }
+            }            
+        }
+        public static void CreateCSV<T>(List<T> list, string filePath)
+        {
+            using (StreamWriter sw = new StreamWriter(filePath))
+            {
+                CreateHeader(list, sw);
+                CreateRows(list, sw);
+            }
+        }
+        private static void CreateHeader<T>(List<T> list, StreamWriter sw)
+        {
+            PropertyInfo[] properties = typeof(T).GetProperties();
+            for (int i = 0; i < properties.Length - 1; i++)
+            {
+                sw.Write(properties[i].Name + ",");
+            }
+            var lastProp = properties[properties.Length - 1].Name;
+            sw.Write(lastProp + sw.NewLine);
+        }
+        private static void CreateRows<T>(List<T> list, StreamWriter sw)
+        {
+            foreach (var item in list)
+            {
+                PropertyInfo[] properties = typeof(T).GetProperties();
+                for (int i = 0; i < properties.Length - 1; i++)
+                {
+                    var prop = properties[i];
+                    sw.Write(prop.GetValue(item) + ",");
+                }
+                var lastProp = properties[properties.Length - 1];
+                sw.Write(lastProp.GetValue(item) + sw.NewLine);
+            }
+        }
+        static void Main(string[] args)
+        {
+            string wd = System.IO.Directory.GetCurrentDirectory();
+
+            ToJson(@"C:\Users\bbergstr\Documents\WorkBench\c#\LizTimeCard\timeCard2.xlsx", @"C:\Users\bbergstr\Documents\WorkBench\c#\LizTimeCard\pog.csv");
+
+            //var csv = new List<string[]>(); // or, List<YourClass>
+            //var lines = System.IO.File.ReadAllLines(@"C:\Users\bbergstr\Documents\WorkBench\c#\LizTimeCard\timeCard.csv");
+            //foreach (string line in lines)
+            //    csv.Add(line.Split(',')); // or, populate YourClass          
+            //string json = new System.Web.Script.Serialization.JavaScriptSerializer().Serialize(csv);
+
+            int derp = 5;
+        }
+    }
+}
